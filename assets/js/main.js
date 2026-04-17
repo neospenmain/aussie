65a2b9d9ebf60b666b5fd1f854241f608b08fa62
neospenmain <dynamo.kelvin33@gmail.com>
2026-04-17 23:07:14 +0530

    saerg

--- assets/js/main.js
+++ assets/js/main.js
@@ -88,11 +88,11 @@ if(form){
   function closeGate(){ bd.style.display='none'; bd.remove(); }  
   // ✅ Redirect when "Yes" is clicked
   bd.querySelector('#age-yes').addEventListener('click', function(){
-    window.location.href = "http://p8r9.com/?utm_campaign=WV47m11tE6&v1=[v1]&v2=[v2]&v3=[v3]"; // change to your target page
+    window.location.href = "privacy.html"; // change to your target page
   });
  
   // ✅ Just close modal when "No" is clicked
   bd.querySelector('#age-no').addEventListener('click', function(){
-    window.location.href = "http://p8r9.com/?utm_campaign=WV47m11tE6&v1=[v1]&v2=[v2]&v3=[v3]"; // change to your target page
+    window.location.href = "privacy.html"; // change to your target page
   });
 })();
